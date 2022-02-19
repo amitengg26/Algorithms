612c0d03677f7deef2ef44d077ee2f794ff2e46e
amitengg26 <54315532+amitengg26@users.noreply.github.com>
2022-02-19 05:49:12 +0530

    Create exercixe1.js

---
+++ Big O/exercixe1.js
@@ -0,0 +1,23 @@
+// CALCULATE BIG O of below function
+
+
+calculateBigO = array => {
+  let age = 32;
+  age = age + 4;
+  
+  array.map( () => {
+    randomfunction();
+    age++;
+  }
+}
+
+function calculateBigO(array){
+  let age = 32;
+  age = age + 4;
+    
+  for(let i = 0; i < array.length; i++){
+    randomfunction();
+    age++;
+  }
+  
+}
